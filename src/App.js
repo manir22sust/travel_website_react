c4b4940a55582c1c433818797d2f6d2ef65cd4fe
manir22sust <manir22sust@gmail.com>
2020-10-07 22:53:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Pages/Home';
+
+function App() {
+  return (
+    <>
+  
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component = 
+          {Home}
+          />
+          
+        </Switch>
+      </Router>
+    </>
+    
+  );
+}
+
+export default App;
